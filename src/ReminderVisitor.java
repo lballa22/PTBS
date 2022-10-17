bdd1515687a5df1f4dc93cdc3ea79431c85e90de
Loka Kalyan Balla <lballa@asu.edu>
2022-10-17 00:11:59 -0700

    source file

---
+++ src/ReminderVisitor.java
@@ -0,0 +1,15 @@
+public class ReminderVisitor extends NodeVisitor {
+
+	public void visitProduct(Product product) {
+
+	}
+
+	public void visitTrading(Trading trading) {
+
+	}
+
+	public void visitFacade(Facade facade) {
+
+	}
+
+}
