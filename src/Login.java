8559953af357d0f910011bfb08d547ffdc8cb22e
Loka Kalyan Balla <lballa@asu.edu>
2022-10-20 12:11:08 -0700

    fixed errors

--- src/Login.java
+++ src/Login.java
@@ -11,71 +11,72 @@ import java.util.Map;
 
 public class Login extends JPanel implements ActionListener{
     public static int UserType = -1;
-    JLabel passwordl;
+    JLabel pwdl;
     JLabel userl;
     JTextField username;
     JButton button,button1;
-    JPasswordField Password;
-    JRadioButton b,se,x,y;
+    JPasswordField pwd;
+    JRadioButton buy,sell,x,y;
     Person p1;
 
     Login(){
         setLayout(null);
 
-        b = new JRadioButton("Buyer");
-        b.setBounds(100,150,120,50);
+        buy = new JRadioButton("Buyer");
+        buy.setBounds(500,350,120,50);
 
-        se = new JRadioButton("Seller");
-        se.setBounds(250,150,120,50);
+        sell = new JRadioButton("Seller");
+        sell.setBounds(620,350,120,50);
 
         userl = new JLabel("Username");
-        userl.setBounds(100, 8, 70, 20);
+        userl.setBounds(500, 208, 70, 20);
 
         username = new JTextField();
-        username.setBounds(100, 27, 193, 28);
+        username.setBounds(500, 227, 193, 28);
 
-        passwordl = new JLabel("Password");
-        passwordl.setBounds(100, 55, 70, 20);
+        pwdl = new JLabel("Password");
+        pwdl.setBounds(500, 255, 70, 20);
 
-        Password = new JPasswordField();
-        Password.setBounds(100, 75, 193, 28);
+        pwd = new JPasswordField();
+        pwd.setBounds(500, 275, 193, 28);
 
         button = new JButton("Login");
-        button.setBounds(100, 110, 90, 25);
+        button.setBounds(500, 310, 90, 25);
         button.addActionListener(this);
 
         addComp();
     }
     public void addComp(){
-        add(b);
-        add(se);
+        add(buy);
+        add(sell);
         add(userl);
         add(username);
-        add(passwordl);
-        add(Password);
+        add(pwdl);
+        add(pwd);
         add(button);
         ButtonGroup ut = new ButtonGroup();
-        ut.add(b);
-        ut.add(se);
+        ut.add(buy);
+        ut.add(sell);
     }
     @Override
     public void actionPerformed(ActionEvent e) {
         try {
             String Username = username.getText();
-            String password = new String(Password.getPassword());
+            String password = new String(pwd.getPassword());
             login(Username, password);
         } catch (IOException ex) {
             throw new RuntimeException(ex);
         }
     }
 
-    public boolean login(String username, String Password) throws IOException {
+    public boolean login(String username, String password) throws IOException {
         File file = null;
-        if(b.isSelected()) {
+        if(buy.isSelected()) {
             file = new File("PTBS/src/resources/buyer.txt");
-        } else if (se.isSelected()) {
+        } else if (sell.isSelected()) {
             file = new File("PTBS/src/resources/seller.txt");
         }
+
         BufferedReader br = new BufferedReader(new FileReader(file));
         Map<String, String> Credentials = new HashMap<>();
         String s;
@@ -83,14 +84,20 @@ public class Login extends JPanel implements ActionListener{
             String[] Cred = s.split(":");
             Credentials.put(Cred[0], Cred[1]);
         }
-        if (Credentials.containsKey(username) && Credentials.get(username).equals(Password)) {
+        if (Credentials.containsKey(username) && Credentials.get(username).equals(password)) {
             System.out.println("Logged in");
             JLabel jl= new JLabel("Logged in successfully please close GUI to proceed further");
             add(jl);
-            jl.setBounds(500,500,100,100);
+            jl.setBounds(0,0,0,0);
+            jl.setBounds(500,500,1000,100);
             revalidate();
             return true;
         }
-        return true;
+        else{
+            System.out.println("Invalid credentials");
+            System.exit(0);
+            return false;
+
+        }
     }
 }
