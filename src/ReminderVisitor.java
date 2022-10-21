b50ebbf5c470b9b76a12289d4b0ed736344853ca
Loka Kalyan Balla <lballa@asu.edu>
2022-10-20 23:39:06 -0700

    added comments

--- src/ReminderVisitor.java
+++ src/ReminderVisitor.java
@@ -19,7 +19,7 @@ public class ReminderVisitor extends NodeVisitor {
 			BufferedReader br = new BufferedReader(new FileReader(file));
 			String line;
 			System.out.println("\t\t*****Visitor Pattern*****"); // Visitor Pattern implementation starts here
-		System.out.println("Products to be traded for"+loggedInUser);
+		System.out.println("Products to be traded for "+loggedInUser);
 			while ((line = br.readLine()) != null) {
 				String[] split = line.split(":");
 				if (split[0].equals(loggedInUser)) {
